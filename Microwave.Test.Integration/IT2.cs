cee5408e1ec0f946a3019942c68bc7ef2c3fc9b5
Egarder <61559853+Egarder@users.noreply.github.com>
2021-04-21 10:18:17 +0200

    dd

--- Microwave.Test.Integration/IT2.cs
+++ Microwave.Test.Integration/IT2.cs
@@ -1,30 +1,45 @@
 using System.Xml.Linq;
 using Microwave.Classes.Boundary;
 using Microwave.Classes.Controllers;
+using Microwave.Classes.Interfaces;
+using NSubstitute;
 using NUnit.Framework;
 
 namespace Microwave.Test.Integration
 {
     public class IT2
     {
-        //private Door _door;
-        //private Button _button;
-        //private Light _light;
-        //private Display _display;
-        //private Output _output;
-        //private PowerTube _powerTube;
-        //private Timer _timer;
+        private Door _door;
+        private Button _powerButton;
+        private Button _timerButton;
+        private Button _startCancelButton;
+        private Light _light;
+        private IDisplay _displayFake;
+        private IOutput _outputFake;
+        private IPowerTube _powerTubeFake;
+        private ITimer _timerFake;
+        private UserInterface _userInterface;
         private CookController _cookController;
 
         [SetUp]
         public void Setup()
         {
-
+            _door = new Door();
+            _powerButton = new Button();
+            _timerButton = new Button();
+            _startCancelButton = new Button();
+            _outputFake = Substitute.For<IOutput>();
+            _light = new Light(_outputFake);
+            _displayFake = Substitute.For<IDisplay>();
+            _powerTubeFake = Substitute.For<IPowerTube>();
+            _timerFake = Substitute.For<ITimer>();
+            _cookController = new CookController(_timerFake,_displayFake,_powerTubeFake);
+            _userInterface = new UserInterface(_powerButton, _timerButton, _startCancelButton,_door,_displayFake,_light,_cookController);
         }
 
         // Emil
         [Test]
-        public void Test1()
+        public void MainScenario()
         {
             Assert.Pass();
         }
