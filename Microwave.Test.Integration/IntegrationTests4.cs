0d3326fa7452fce71bb986fe80eec33cf3e430ae
Simon Kroer <au460826@uni.au>
2021-04-22 09:28:03 +0200

    started IT4

--- Microwave.Test.Integration/IntegrationTests4.cs
+++ Microwave.Test.Integration/IntegrationTests4.cs
@@ -1,32 +1,343 @@
 using System.Xml.Linq;
 using Microwave.Classes.Boundary;
 using Microwave.Classes.Controllers;
+using Microwave.Classes.Interfaces;
 using NUnit.Framework;
 
 namespace Microwave.Test.Integration
 {
     public class Tests4
     {
-        //private Door _door;
-        //private Button _button;
-        //private Light _light;
-        //private Display _display;
-        //private Output _output;
-        //private PowerTube _powerTube;
-        //private Timer _timer;
-        //private CookController _cookController;
+        private UserInterface sut;
 
+        private Button powerButton;
+        private Button timeButton;
+        private Button startCancelButton;
+
+        private Door door;
+
+        private Display display;
+        private Light light;
+
+        private CookController cooker;
+        private Output output;
+        private Timer timer;
+        private PowerTube powerTube;
         [SetUp]
         public void Setup()
         {
-
+            powerButton = new Button();
+            timeButton = new Button();
+            startCancelButton = new Button();
+            door = new Door();
+            timer = new Timer();
+            powerTube = new PowerTube(output);
+            light = new Light(output);
+            display = new Display(output);
+            cooker = new CookController(timer, display, powerTube);
+            sut = new UserInterface(powerButton, timeButton, startCancelButton, door, display, light, cooker);
+            cooker.UI = sut;
         }
 
         // Simon
         [Test]
-        public void Test1()
+        public void Ready_DoorOpen_LightOnShowsOnOutput()
         {
-            Assert.Pass();
+            // This test that uut has subscribed to door opened, and works correctly
+            // simulating the event through NSubstitute
+            door.Open()
+            output.r
         }
+
+        [Test]
+        public void DoorOpen_DoorClose_LightOff()
+        {
+            // This test that uut has subscribed to door opened and closed, and works correctly
+            // simulating the event through NSubstitute
+            door.Opened += Raise.EventWith(this, EventArgs.Empty);
+            door.Closed += Raise.EventWith(this, EventArgs.Empty);
+            light.Received().TurnOff();
+        }
+
+        [Test]
+        public void Ready_DoorOpenClose_Ready_PowerIs50()
+        {
+            // This test that uut has subscribed to power button, and works correctly
+            // simulating the events through NSubstitute
+            door.Opened += Raise.EventWith(this, EventArgs.Empty);
+            door.Closed += Raise.EventWith(this, EventArgs.Empty);
+
+            powerButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            display.Received(1).ShowPower(Arg.Is<int>(50));
+        }
+
+        [Test]
+        public void Ready_2PowerButton_PowerIs100()
+        {
+            powerButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            powerButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            display.Received(1).ShowPower(Arg.Is<int>(100));
+        }
+
+        [Test]
+        public void Ready_14PowerButton_PowerIs700()
+        {
+            for (int i = 1; i <= 14; i++)
+            {
+                powerButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            }
+            display.Received(1).ShowPower(Arg.Is<int>(700));
+        }
+
+        [Test]
+        public void Ready_15PowerButton_PowerIs50Again()
+        {
+            for (int i = 1; i <= 15; i++)
+            {
+                powerButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            }
+            // And then once more
+            powerButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            display.Received(2).ShowPower(50);
+        }
+
+        [Test]
+        public void SetPower_CancelButton_DisplayCleared()
+        {
+            // Also checks if TimeButton is subscribed
+            powerButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            // Now in SetPower
+            startCancelButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+
+            display.Received(1).Clear();
+        }
+
+        [Test]
+        public void SetPower_DoorOpened_DisplayCleared()
+        {
+            // Also checks if TimeButton is subscribed
+            powerButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            // Now in SetPower
+            door.Opened += Raise.EventWith(this, EventArgs.Empty);
+
+            display.Received(1).Clear();
+        }
+
+        [Test]
+        public void SetPower_DoorOpened_LightOn()
+        {
+            // Also checks if TimeButton is subscribed
+            powerButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            // Now in SetPower
+            door.Opened += Raise.EventWith(this, EventArgs.Empty);
+
+            light.Received(1).TurnOn();
+        }
+
+        [Test]
+        public void SetPower_TimeButton_TimeIs1()
+        {
+            // Also checks if TimeButton is subscribed
+            powerButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            // Now in SetPower
+            timeButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+
+            display.Received(1).ShowTime(Arg.Is<int>(1), Arg.Is<int>(0));
+        }
+
+        [Test]
+        public void SetPower_2TimeButton_TimeIs2()
+        {
+            powerButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            // Now in SetPower
+            timeButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            timeButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+
+            display.Received(1).ShowTime(Arg.Is<int>(2), Arg.Is<int>(0));
+        }
+
+        [Test]
+        public void SetTime_StartButton_CookerIsCalled()
+        {
+            powerButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            // Now in SetPower
+            timeButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            // Now in SetTime
+            startCancelButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+
+            cooker.Received(1).StartCooking(50, 60);
+        }
+
+        [Test]
+        public void SetTime_DoorOpened_DisplayCleared()
+        {
+            powerButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            // Now in SetPower
+            timeButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            // Now in SetTime
+            door.Opened += Raise.EventWith(this, EventArgs.Empty);
+
+            display.Received().Clear();
+        }
+
+        [Test]
+        public void SetTime_DoorOpened_LightOn()
+        {
+            powerButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            // Now in SetPower
+            timeButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            // Now in SetTime
+            door.Opened += Raise.EventWith(this, EventArgs.Empty);
+
+            light.Received().TurnOn();
+        }
+
+        [Test]
+        public void Ready_PowerAndTime_CookerIsCalledCorrectly()
+        {
+            powerButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            // Now in SetPower
+            powerButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+
+            timeButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            // Now in SetTime
+            timeButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+
+            // Should call with correct values
+            startCancelButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+
+            cooker.Received(1).StartCooking(100, 120);
+        }
+
+        [Test]
+        public void Ready_FullPower_CookerIsCalledCorrectly()
+        {
+            for (int i = 50; i <= 700; i += 50)
+            {
+                powerButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            }
+
+            timeButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            // Now in SetTime
+
+            // Should call with correct values
+            startCancelButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+
+            cooker.Received(1).StartCooking(700, 60);
+
+        }
+
+
+        [Test]
+        public void SetTime_StartButton_LightIsCalled()
+        {
+            powerButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            // Now in SetPower
+            timeButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            // Now in SetTime
+            startCancelButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            // Now cooking
+
+            light.Received(1).TurnOn();
+        }
+
+        [Test]
+        public void Cooking_CookingIsDone_LightOff()
+        {
+            powerButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            // Now in SetPower
+            timeButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            // Now in SetTime
+            startCancelButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            // Now in cooking
+
+            uut.CookingIsDone();
+            light.Received(1).TurnOff();
+        }
+
+        [Test]
+        public void Cooking_CookingIsDone_ClearDisplay()
+        {
+            powerButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            // Now in SetPower
+            timeButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            // Now in SetTime
+            startCancelButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            // Now in cooking
+
+            // Cooking is done
+            uut.CookingIsDone();
+            display.Received(1).Clear();
+        }
+
+        [Test]
+        public void Cooking_DoorIsOpened_CookerCalled()
+        {
+            powerButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            // Now in SetPower
+            timeButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            // Now in SetTime
+            startCancelButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            // Now in cooking
+
+            // Open door
+            door.Opened += Raise.EventWith(this, EventArgs.Empty);
+
+            cooker.Received(1).Stop();
+        }
+
+        [Test]
+        public void Cooking_DoorIsOpened_DisplayCleared()
+        {
+            powerButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            // Now in SetPower
+            timeButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            // Now in SetTime
+            startCancelButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            // Now in cooking
+
+            // Open door
+            door.Opened += Raise.EventWith(this, EventArgs.Empty);
+
+            display.Received(1).Clear();
+        }
+
+        [Test]
+        public void Cooking_CancelButton_CookerCalled()
+        {
+            powerButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            // Now in SetPower
+            timeButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            // Now in SetTime
+            startCancelButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            // Now in cooking
+
+            // Open door
+            startCancelButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+
+            cooker.Received(1).Stop();
+        }
+
+        [Test]
+        public void Cooking_CancelButton_LightCalled()
+        {
+            powerButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            // Now in SetPower
+            timeButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            // Now in SetTime
+            startCancelButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            // Now in cooking
+
+            // Open door
+            startCancelButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+
+            light.Received(1).TurnOff();
+        }
+
+
+    }
+
+}
+
     }
 }
